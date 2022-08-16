fd6ffa273c4beb8fadeeb98bbd67016099d5524e
JennysVoods <JennysVoods@outlook.com@git config --global user.name JennysVoodsgit config --global user.email JennysVoods@outlook.comgit config --global user.email JennysVoods@outlook.com@>
2022-08-16 19:42:55 +0300

    добавлено решение задачи

--- ThreeSymbol/Program.cs
+++ ThreeSymbol/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void ThreeSymbol(string[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string str = arr[i];
+        if(str.Length <= 3) Console.WriteLine(str);
+    }
+}
+
+
+string[] array = {"hello", "2", "world", ":-)"};
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+ThreeSymbol(array);
